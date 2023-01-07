71110bf63791bb2389f514d1262e7673b9e3ef2e
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-01-06 21:11:40 -0300

    Multiple changes Added: - getters - method checkIsSupportedStateOrThrow
    Refactored:
    - idiomatic error checking with InvalidElementTypeError.check
    - included semi-colons

--- src/scripts/StateElement.ts
+++ src/scripts/StateElement.ts
@@ -1,4 +1,4 @@
-import { ElementHaventStateError, StateNotSupportedError } from './Errors.js';
+import { ElementHaventStateError, StateNotSupportedError, InvalidElementTypeError } from './Errors.js';
 
 /**
  * A way to separate the HTML in states/steps.
@@ -28,9 +28,9 @@ import { ElementHaventStateError, StateNotSupportedError } from './Errors.js';
  * called `for-state` in it, with the desired state.
  */
 export class StateElement {
-    private rootElement: HTMLElement;
-    private supportedStates: string[];
-    private childStateItems: HTMLElement[] = [];
+    private _rootElement: HTMLElement;
+    private _supportedStates: string[];
+    private _childStateItems: HTMLElement[] = [];
     private currentState: string;
 
     // Keys: state name
@@ -38,12 +38,12 @@ export class StateElement {
     private stateItems = new Map<string, HTMLElement[]>();
 
     constructor(element: HTMLElement, supportedStates: string[], initialState?: string) {
-        this.rootElement = element;
-        this.supportedStates = supportedStates;
+        this._rootElement = element;
+        this._supportedStates = supportedStates;
 
         const attributeState = this.validateElementHaveAttributeAndReturnIt();
 
-        if (!this.checkIsSupportedState(attributeState)) throw new StateNotSupportedError(attributeState);
+        this.checkIsSupportedStateOrThrow(attributeState);
         this.currentState = attributeState;
 
         this.populateStateItems();
@@ -58,26 +58,27 @@ export class StateElement {
 
     // #region private
     private validateElementHaveAttributeAndReturnIt() {
-        const attribute = this.rootElement.getAttribute('data-state');
+        const attribute = this._rootElement.getAttribute('data-state');
         if (attribute) return attribute;
+
         throw new ElementHaventStateError;
     }
 
     private populateStateItems() {
-        for (const state of this.supportedStates) {
+        for (const state of this._supportedStates) {
             this.stateItems.set(state, []);
         }
     }
 
     private searchForStateItems() {
-        const { children } = this.rootElement;
+        const { children } = this._rootElement;
 
         // Faster than copying the collection to a new array.
         for (let i = 0; i < children.length; i++) {
             const element = children.item(i);
 
             if (element === null) continue;
-            if (!(element instanceof HTMLElement)) continue
+            if (!(element instanceof HTMLElement)) continue;
 
             const stateName = element.getAttribute('for-state');
 
@@ -85,18 +86,36 @@ export class StateElement {
             if (!this.checkIsSupportedState(stateName)) continue;
 
             this.stateItems.get(stateName)?.push(element);
-            this.childStateItems.push(element);
+            this._childStateItems.push(element);
         }
     }
     // #endregion
 
     // #region public
+    get rootElement() {
+        return this._rootElement;
+    }
+    get childStateItems() {
+        return this._childStateItems;
+    }
+    get supportedStates() {
+        return this._supportedStates;
+    }
+    get state() {
+        return this.currentState;
+    }
+
     checkIsSupportedState(name: string): boolean {
-        return this.supportedStates.indexOf(name) !== -1;
+        return this._supportedStates.indexOf(name) !== -1;
+    }
+    checkIsSupportedStateOrThrow(name: string) {
+        if (this.checkIsSupportedState(name)) return;
+
+        throw new StateNotSupportedError(name);
     }
 
     updateItemsVisibility() {
-        for (const item of this.childStateItems) {
+        for (const item of this._childStateItems) {
             if (item.getAttribute('for-state') === this.currentState) {
                 item.style.display = '';
             } else {
@@ -106,7 +125,7 @@ export class StateElement {
     }
 
     changeStateTo(stateName: string) {
-        if (!this.checkIsSupportedState(stateName)) throw new StateNotSupportedError(stateName);
+        this.checkIsSupportedStateOrThrow(stateName);
 
         this.currentState = stateName;
         this.updateItemsVisibility();
@@ -121,10 +140,12 @@ export class StateElement {
 
         const element = result.item(0);
 
-        if (!(element instanceof HTMLElement)) throw new TypeError(`Element from selector "${cssSelector}" is not an HTMLElement`);
+        InvalidElementTypeError.check(element, HTMLElement);
+
         if (result.length > 1) console.warn(`There was found other ${result.length - 1} elements with selector ${cssSelector}`);
 
-        return new StateElement(element, supportedStates);
+        // Its beeing checked for HTMLElement at InvalidElementTypeError.check
+        return new StateElement(element as HTMLElement, supportedStates);
     }
     // #endregion
 }
