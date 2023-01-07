d8e6a739d2fd52653465875b57f714279f9026be
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-01-06 21:14:57 -0300

    Added temporary changes to the page

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,13 +1,15 @@
+import { AttributeObserver } from './AttributeObserver.js';
 import { FileSelector } from './FileSelector.js';
 import { StateElement } from './StateElement.js';
 
-const headerState = StateElement.fromSelector(
-    'body[data-state]',
-    ['intro', 'file-view']
-);
-const formState = StateElement.fromSelector(
-    'body main form[data-state]',
-    ['waiting-file', 'file-selected', 'analyzing', 'error']
-);
+const validHeaderStates = ['intro', 'file-view'];
+const validFormStates = ['waiting-file', 'file-selected', 'analyzing', 'error'];
+
+const headerState = StateElement.fromSelector('body[data-state]', validHeaderStates);
+const formState = StateElement.fromSelector('body main form[data-state]', validFormStates);
+
+const formAttObserver = new AttributeObserver(formState.rootElement);
+
+formAttObserver.observe(console.log);
 
 const fileSelector = new FileSelector('form.fileSelector');
