89c968a8fcf451f931cc222bda7832323ef39753
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-01-09 13:31:54 -0300

    Multiple modifications Added: - interface `FileSelectorStates` as type for propery `stateItems` to store each state element; - object property `validStates` to store state names; - method `formQueryOrThrow` to select and check child element from form; - event switches to waiting file, file selected and error; - listener for input.
    Removed:
    - enum `SelectroStates`;
    
    Refactor:
    - constructor now accepts an element instead query string (responsability moved to static method `fromSelector`);
    - naming;
    - removed useless code at index.ts.

--- src/scripts/FileSelector.ts
+++ src/scripts/FileSelector.ts
@@ -1,61 +1,158 @@
-const enum SelectorStates {
-    WAITING, HOVERING, SELECTED, ANALYZING, ERROR
+import { InvalidElementTypeError } from "./Errors.js";
+import { StateElement } from "./StateElement.js";
+
+interface FileSelectorStates {
+    waitingFile: HTMLLabelElement;
+    fileSelected: HTMLLabelElement;
+    analyzingFIle: HTMLLabelElement;
+    error: HTMLLabelElement;
 }
 
 export class FileSelector {
-    private form: HTMLFormElement;
-    private labelWaiting: HTMLLabelElement;
-    private labelSelected: HTMLLabelElement;
-    private state: SelectorStates;
+    readonly validStates = {
+        WAITING: 'waiting-file',
+        SELECTED: 'file-selected',
+        ANALYZING: 'analyzing',
+        ERROR: 'error'
+    };
 
-    constructor(elementSelector: string) {
-        const formEl = document.querySelector<HTMLFormElement>(elementSelector);
-        if (formEl === null) throw new TypeError('Form element not found');
-        this.form = formEl;
+    private fileInput: HTMLInputElement;
 
-        const labelWaitingEl = formEl.querySelector<HTMLLabelElement>('label[on-waiting]');
-        if (labelWaitingEl === null) throw new TypeError('"label waiting" element not found');
-        this.labelWaiting = labelWaitingEl;
+    private stateItems: FileSelectorStates;
+    private statefulForm: StateElement;
 
-        const labelSelectedEl = formEl.querySelector<HTMLLabelElement>('label[on-selected]');
-        if (labelSelectedEl === null) throw new TypeError('"label selected" element not found');
-        this.labelSelected = labelSelectedEl;
+    constructor(formElement: HTMLFormElement) {
+        this.statefulForm = new StateElement(
+            formElement,
+            this.validStateList,
+            this.validStates.WAITING
+        );
 
-        this.state = SelectorStates.WAITING;
+        this.stateItems = {
+            waitingFile: this.formQueryOrThrow('label[for-state="waiting-file"]'),
+            fileSelected: this.formQueryOrThrow('label[for-state="file-selected"]'),
+            analyzingFIle: this.formQueryOrThrow('label[for-state="analyzing"]'),
+            error: this.formQueryOrThrow('label[for-state="error"]')
+        };
+
+        this.fileInput = this.formQueryOrThrow('input');
 
         this.attachListeners();
+        this.switchToWaitingFileState();
+    }
+
+    // #region static
+    /** @throws {TypeError, InvalidElementTypeError} */
+    static fromSelector(cssSelector: string) {
+        const result = document.querySelectorAll(cssSelector);
+
+        if (result.length === 0) throw new TypeError(`Not found elements with selector "${cssSelector}"`);
+
+        const element = result.item(0);
+
+        InvalidElementTypeError.check(element, HTMLFormElement);
+
+        if (result.length > 1) console.warn(`There was found other ${result.length - 1} elements with selector ${cssSelector}`);
+
+        // Its beeing checked for HTMLFormElement at InvalidElementTypeError.check
+        return new FileSelector(element as HTMLFormElement);
+    }
+    // #endregion
+
+    // #region private
+    /** @throws {Error} */
+    private formQueryOrThrow<E extends Element = Element>(selector: string): E {
+        const element = this.statefulForm.root.querySelector<E>(selector);
+        if (!element) throw new Error(`Mandatory element by selector "${selector}" not found inside form`);
+        return element;
     }
 
     private attachListeners() {
-        this.labelWaiting.addEventListener('dragover', this.whenFileIsOverSelector.bind(this));
-        this.labelWaiting.addEventListener('dragleave', this.whenFileHoverOffSelector.bind(this));
-        this.labelWaiting.addEventListener('drop', this.whenFileDropped.bind(this));
+        this.fileInput.addEventListener('input', this.whenFileSelected.bind(this));
+
+        this.stateItems.waitingFile.addEventListener('dragover', this.whenFileIsOverSelector.bind(this));
+        this.stateItems.waitingFile.addEventListener('dragleave', this.whenFileHoverOffSelector.bind(this));
+        this.stateItems.waitingFile.addEventListener('drop', this.whenFileDropped.bind(this));
     }
 
     private whenFileIsOverSelector(event: DragEvent) {
         event.preventDefault();
 
-        this.state = SelectorStates.HOVERING;
-
-        this.form.classList.add('hovering-file');
+        this.fileHover(true);
     }
-    private whenFileHoverOffSelector() {
-        this.state = SelectorStates.WAITING;
+    private whenFileHoverOffSelector(event: DragEvent) {
+        event.preventDefault();
 
-        this.form.classList.remove('hovering-file');
+        this.fileHover(false);
     }
     private whenFileDropped(event: DragEvent) {
         event.preventDefault();
 
-        this.state = SelectorStates.SELECTED;
-        console.log(event.dataTransfer?.files);
+        const { dataTransfer } = event;
 
-        if (event.dataTransfer) {
-            for (let i = 0; i < event.dataTransfer.files.length; i++) {
-                console.log(event.dataTransfer.files[i]);
-            }
+        if (!dataTransfer) {
+            this.switchToErrorState('Error on dataTransfer');
+            console.log(dataTransfer);
+            return;
         }
 
-        this.form.classList.remove('hovering-file');
+        this.switchToFileSelectedState(dataTransfer.files);
+        this.fileHover(false);
     }
+    private whenFileSelected(event: Event) {
+        const { files } = this.fileInput;
+
+        if (!files) {
+            this.switchToErrorState('Ocurred an error while loading the file');
+            return;
+        }
+
+        this.switchToFileSelectedState(files);
+    }
+
+    private fileHover(enable: boolean) {
+        if (enable) {
+            this.statefulForm.root.classList.add('hovering-file');
+        } else {
+            this.statefulForm.root.classList.remove('hovering-file');
+        }
+    }
+
+    private switchToWaitingFileState() {
+        // Just to garantee
+        this.fileHover(false);
+
+        this.statefulForm.changeStateTo(this.validStates.WAITING);
+    }
+    private switchToFileSelectedState(files: FileList) {
+        const filenameBEl = this.stateItems.fileSelected.querySelector<HTMLElement>('b.filename');
+        if (!filenameBEl) throw new TypeError('b.filename not found: is null');
+
+        const file = files.item(0);
+
+        if (!file) {
+            this.switchToErrorState('File item index 0 not found');
+            return;
+        }
+
+        filenameBEl.innerText = file.name;
+        console.log(files);
+
+        this.statefulForm.changeStateTo(this.validStates.SELECTED);
+    }
+    private switchToErrorState(errorMessage: string) {
+        const pEl = this.stateItems.error.querySelector<HTMLParagraphElement>('p.errorMessage');
+        if (!pEl) throw new TypeError('p.errorMessage not found: is null');
+
+        pEl.innerText = errorMessage;
+
+        this.statefulForm.changeStateTo(this.validStates.ERROR);
+    }
+    // #endregion
+
+    // #region public
+    get validStateList() {
+        return Object.values(this.validStates);
+    }
+    // #endregion
 }
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,15 +1,7 @@
-import { AttributeObserver } from './AttributeObserver.js';
 import { FileSelector } from './FileSelector.js';
 import { StateElement } from './StateElement.js';
 
 const validHeaderStates = ['intro', 'file-view'];
-const validFormStates = ['waiting-file', 'file-selected', 'analyzing', 'error'];
-
 const headerState = StateElement.fromSelector('body[data-state]', validHeaderStates);
-const formState = StateElement.fromSelector('body main form[data-state]', validFormStates);
 
-const formAttObserver = new AttributeObserver(formState.rootElement);
-
-formAttObserver.observe(console.log);
-
-const fileSelector = new FileSelector('form.fileSelector');
+const fileSelector = FileSelector.fromSelector('form.fileSelector');
