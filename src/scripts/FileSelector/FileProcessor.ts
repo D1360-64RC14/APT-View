24a185ae159c8dbb84fd34bdc1df8f0512072e34
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-02-04 20:50:55 -0300

    `FileAnalyzer` class renamed to `FileProcessor`. Also file.

--- src/scripts/FileSelector/FileProcessor.ts
+++ src/scripts/FileSelector/FileProcessor.ts
@@ -1,16 +1,16 @@
 import { APTAPI } from "../APTAPI/APTAPI.js";
 import { FileController } from "./FileController.js";
 
-export class FileAnalyzer {
+export class FileProcessor {
     private aptapi = new APTAPI;
 
-    constructor(private fileController: FileController) {
-        fileController.observeFile(this.whenFileSelected.bind(this));
-    }
+    constructor(private fileController: FileController) { }
 
-    private async whenFileSelected(file: File) {
+    async process(file: File) {
         const result = await this.aptapi.processFile(file);
 
         console.log(result);
+
+
     }
 }
