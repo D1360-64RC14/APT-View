8ef827fd8b5b87f0454d89f7771e87af2b4feb23
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-02-13 11:36:58 -0300

    Access to action amounts and commands groupped by day implemented to APTAPI

--- src/scripts/APTAPI/APTAPI.ts
+++ src/scripts/APTAPI/APTAPI.ts
@@ -1,7 +1,59 @@
+import { MapTools } from "../Tools/MapTools.js";
+import { Command } from "./Command.js";
+import { CommandDayGroup } from "./CommandDayGroup.js";
 import { HistoryFileProcessor } from "./HistoryFileProcessor.js";
 
 export class APTAPI {
-    processFile(file: File) {
-        return HistoryFileProcessor.process(file);
+    private commands = new Array<Command>;
+    private _actionAmounts?= new Map<string, number>;
+    private commandsGroupedByDay?= new Array<CommandDayGroup>;
+
+    async loadFile(file: File) {
+        this.commands = await HistoryFileProcessor.process(file);
+
+        this.sortCommands();
+        this.calculateActionAmounts();
+        this.calculateDayGroups();
+    }
+
+    private sortCommands() {
+        this.commands.sort(APTAPI.sortCommandByStartDate);
+    }
+
+    static sortCommandByStartDate(a: Command, b: Command) {
+        return a.startDate.getTime() - b.startDate.getTime();
+    }
+
+    private calculateActionAmounts() {
+        const bufferAmounts = new Map<string, number>;
+
+        for (const command of this.commands) {
+            command.actions.forEach(action => {
+                MapTools.incrementValueFrom(bufferAmounts, action.name);
+            });
+        }
+
+        this._actionAmounts = bufferAmounts;
+    }
+
+    private calculateDayGroups() {
+        this.commandsGroupedByDay = CommandDayGroup.process(this.commands);
+    }
+
+    get actionAmounts() {
+        if (!this._actionAmounts)
+            this.throwObjectNotInitialized();
+
+        return this._actionAmounts;
+    }
+    get dayGroups() {
+        if (!this.commandsGroupedByDay)
+            this.throwObjectNotInitialized();
+
+        return this.commandsGroupedByDay;
+    }
+
+    private throwObjectNotInitialized() {
+        throw new Error('This object hasn\'t yet initialized');
     }
 }
