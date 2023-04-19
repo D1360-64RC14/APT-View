5866245c6cb602ffceebcbb37111ff4728c663eb
D1360-64RC14 <d1360_64rc14@protonmail.com>
2023-04-19 17:51:52 -0300

    WIP Refact

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,7 +1,4 @@
-import { FileSelector } from './FileSelector/FileSelector.js';
 import { StateElement } from './StateElement.js';
 
 const validHeaderStates = ['intro', 'file-view'];
 const headerState = StateElement.fromSelector('body[data-state]', validHeaderStates);
-
-const fileSelector = FileSelector.fromSelector('form.fileSelector');
